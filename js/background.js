486ce323401f46bff261d7100e84ae82c35f0ae3
vegetablechicken-dev <1204694597@qq.com>
2025-01-22 20:12:59 +0800

    Site updated: 2025-01-22 20:12:57

--- js/background.js
+++ js/background.js
@@ -1,4 +1,4 @@
-var img_array = ["url(https://gitee.com/vegetablechicken-dev/pic/raw/master/img/img/1.png)", "url(https://gitee.com/vegetablechicken-dev/pic/raw/master/img/img/2.png)", "url(/img/3.png)", "url(/img/5.png)", "url(https://gitee.com/vegetablechicken-dev/pic/raw/master/img/img/6.png)", "url(https://gitee.com/vegetablechicken-dev/pic/raw/master/img/img/7.png)", "url(https://gitee.com/vegetablechicken-dev/pic/raw/master/img/img/8.png)"];
-var index = new Date().getDay();
+var img_array = ["url(/img/1.png)", "url(https://gitee.com/vegetablechicken-dev/pic/raw/master/img/img/2.png)", "url(/img/3.png)", "url(/img/5.png)", "url(https://gitee.com/vegetablechicken-dev/pic/raw/master/img/img/6.png)", "url(/img/7.png)", "url(https://gitee.com/vegetablechicken-dev/pic/raw/master/img/img/8.png)"];
+var index = Math.floor(Math.random() * img_array.length);
 var current_background = img_array[index];
 document.getElementById("web_bg").style.backgroundImage = current_background;
